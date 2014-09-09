59f0718dffc46f232befd3eabfde4c695a127f3d
Brian Guarraci <bguarraci@twitter.com>
2014-09-09 07:12:59 -0700

    upgrade to scala 2.10

--- src/main/scala/org/ops5/starlisp/lispserver/LispServer.scala
+++ src/main/scala/org/ops5/starlisp/lispserver/LispServer.scala
@@ -8,17 +8,17 @@ object REPL extends NestServer(8080) {
 
   // curl "http://localhost:8080/hello"
   get("/hello") { args =>
-    LispServer.eval("(+ 1 1)")
+    LispServer("(+ 1 1)")
   }
 
   // curl -d"sexp=(+ 2 2)" http://localhost:8080/hello
   post("/hello") { args =>
-    LispServer.eval(args.get("sexp"))
+    LispServer(args.get("sexp"))
   }
 }
 
 object LispServer {
-  def eval(sexp: String): RouteResponse  = {
+  def apply(sexp: String): RouteResponse = {
     val runtime = Runtime.createAndBootstrap
     val stream = runtime.inputStreamFromString(sexp)
     val lispObject = runtime.eval(runtime.read(stream))
